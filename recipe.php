27813838b94d63ce643058832787162a0a4ca268
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-05-22 22:34:29 +0100

    add sub_title to get from database

--- recipe.php
+++ recipe.php
@@ -18,7 +18,7 @@ $recipes = [];
 // $sql = "SELECT * FROM recipes WHERE id = '{$id}'";
 // $sql = "SELECT recipes.id as id, recipes.name as name, recipes.category_id as c_id, recipes.description as description, recipes.ingredients as ingredients, recipes.instructions as instructions, recipes.suggestions as suggestions, categories.name as c_name FROM recipes, categories WHERE recipes.id = '{$id}'";
 // $sql = "SELECT recipes.id as id, recipes.name as name, recipes.category_id as c_id, recipes.description as description, recipes.ingredients as ingredients, recipes.instructions as instructions, recipes.suggestions as suggestions, categories.name as c_name FROM recipes, categories WHERE recipes.category_id = categories.id AND categories.id ='{$id}' ORDER BY categories.name ASC";
-$sql = "SELECT recipes.id as id, recipes.name as name, recipes.category_id as c_id, recipes.description as description, recipes.ingredients as ingredients, recipes.instructions as instructions, recipes.suggestions as suggestions, categories.name as c_name FROM recipes, categories WHERE recipes.category_id = categories.id AND recipes.id ='{$id}' ORDER BY categories.name ASC"; 
+$sql = "SELECT recipes.id as id, recipes.name as name, recipes.sub_title as sub_title ,recipes.category_id as c_id, recipes.description as description, recipes.ingredients as ingredients, recipes.instructions as instructions, recipes.suggestions as suggestions, categories.name as c_name FROM recipes, categories WHERE recipes.category_id = categories.id AND recipes.id ='{$id}' ORDER BY categories.name ASC"; 
 
 
 if($result = mysqli_query($con, $sql))
@@ -28,6 +28,7 @@ if($result = mysqli_query($con, $sql))
   {
     $recipes[$cr]['id'] = $row['id'];
     $recipes[$cr]['name'] = $row['name'];
+    $recipes[$cr]['sub_title'] = $row['sub_title'];
     $recipes[$cr]['c_id'] = $row['c_id'];
     $recipes[$cr]['c_name'] = $row['c_name'];
     $recipes[$cr]['description'] = $row['description'];
